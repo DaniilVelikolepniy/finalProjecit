0537a0cf86ebb85ccda7a7305a0400a90ad0edbb
Данила <51332923+DaniilVelikolepniy@users.noreply.github.com>
2025-07-21 15:30:14 +0500

    навигация

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,16 @@
                         {{ __('Брони') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="{{ route('h.create') }}" active="{{ request()->routeIs('h.create') }}">
+                        {{ __('Добавление отеля') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="{{ route('r.create') }}" active="{{ request()->routeIs('r.create') }}">
+                        {{ __('Добавление комнаты') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
