8436f68039810175922c3c7ee59fc9d631981cd0
Данила <51332923+DaniilVelikolepniy@users.noreply.github.com>
2025-08-08 00:20:58 +0500

    Добавил проверку текущего пользователя аутентификации.

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\RoleController;
 Route::get('/', function () {
     return view('index');
 })
-->middleware('auth')
-->name('home');
+    ->middleware('auth')
+    ->name('home');
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/authUser', [AuthController::class, 'login'])->name('auth');
@@ -26,47 +26,48 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::prefix('roles')
-    ->name('roles.')
-    ->middleware('role:admin')
-    ->group(function () {
+Route::middleware('auth')->group(function () {
 
-        Route::get('/', [RoleController::class, 'index'])->name('index');
-        Route::get('/create', [RoleController::class, 'create'])->name('create');
-        Route::post('/', [RoleController::class, 'store'])->name('store');
-        Route::get('/{role}/edit', [RoleController::class, 'edit'])->name('edit');
-        Route::put('/{role}', [RoleController::class, 'update'])->name('update');
-        Route::get('/{role}/users', [RoleController::class, 'users'])->name('users');
-        Route::delete('/{role}', [RoleController::class, 'destroy'])->name('destroy');
-    });
+    Route::prefix('roles')
+        ->name('roles.')
+        ->middleware('role:admin')
+        ->group(function () {
+            Route::get('/', [RoleController::class, 'index'])->name('index');
+            Route::get('/create', [RoleController::class, 'create'])->name('create');
+            Route::post('/', [RoleController::class, 'store'])->name('store');
+            Route::get('/{role}/edit', [RoleController::class, 'edit'])->name('edit');
+            Route::put('/{role}', [RoleController::class, 'update'])->name('update');
+            Route::get('/{role}/users', [RoleController::class, 'users'])->name('users');
+            Route::delete('/{role}', [RoleController::class, 'destroy'])->name('destroy');
+        });
 
+    Route::resource('/hotels', HotelController::class)->names([
+        'index' => 'h.list',
+        'show' => 'h.show',
+        'create' => 'h.create',
+        'store' => 'h.store',
+        'edit' => 'h.edit',
+        'update' => 'h.update',
+        'destroy' => 'h.destroy',
+    ]);
 
-Route::resource('/hotels', HotelController::class)->names([
-    'index' => 'h.list',
-    'show' => 'h.show',
-    'create' => 'h.create',
-    'store' => 'h.store',
-    'edit' => 'h.edit',
-    'update' => 'h.update',
-    'destroy' => 'h.destroy',
-]);
+    Route::resource('/rooms', RoomController::class)->names([
+        'index' => 'r.index',
+        'show' => 'r.show',
+        'create' => 'r.create',
+        'store' => 'r.store',
+        'edit' => 'r.edit',
+        'update' => 'r.update',
+        'destroy' => 'r.destroy',
+    ]);
 
-Route::resource('/rooms', RoomController::class)->names([
-    'index' => 'r.index',
-    'show' => 'r.show',
-    'create' => 'r.create',
-    'store' => 'r.store',
-    'edit' => 'r.edit',
-    'update' => 'r.update',
-    'destroy' => 'r.destroy',
-]);
-
-Route::resource('/bookings', BookingController::class)->names([
-    'index' => 'b.index',
-    'show' => 'b.show',
-    'create' => 'b.create',
-    'store' => 'b.store',
-    'edit' => 'b.edit',
-    'update' => 'b.update',
-    'destroy' => 'b.destroy',
-]);
+    Route::resource('/bookings', BookingController::class)->names([
+        'index' => 'b.index',
+        'show' => 'b.show',
+        'create' => 'b.create',
+        'store' => 'b.store',
+        'edit' => 'b.edit',
+        'update' => 'b.update',
+        'destroy' => 'b.destroy',
+    ]);
+});
